e37929df5c348490dd2da240e335d11d81cdbd49
Zac Hobbs <zac.hobbs@gmail.com>
2019-05-17 12:59:23 +0100

    price validation added and stars limited from 0 to 5

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,7 @@
 class Review < ApplicationRecord
   belongs_to :bike
   belongs_to :rider, foreign_key: "rider_id", class_name: "User"
-  validates :stars, presence: true
+  validates :stars, presence: true, inclusion: 0..5
   validates :description, presence: true
+  validates :price, presence: true
 end
