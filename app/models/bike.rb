ceb3dd732fffae84d7cfb074e8c348a15afec003
Zac Hobbs <zac.hobbs@gmail.com>
2019-05-17 11:15:18 +0100

    bike validations added

--- app/models/bike.rb
+++ app/models/bike.rb
@@ -1,3 +1,11 @@
 class Bike < ApplicationRecord
   belongs_to :owner, foreign_key: "owner_id", class_name: "User"
+  validates :description, presence: true
+  validates :make, presence: true
+  validates :model, presence: true
+  validates :color, presence: true
+  validates :type, presence: true
+  validates :frame_size, presence: true
+  validates :foldable, presence: true
+  validates :location, presence: true
 end
