6768ee60673dba7df0254c76ec4e4709b70de1b3
Zac Hobbs <zac.hobbs@gmail.com>
2019-05-17 11:16:52 +0100

    user validations added

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :bikes
+  validates :email, presence: true, uniqueness: true
+  validates_format_of :email, :with => Devise::email_regexp
+  validates :username, presence: true, uniqueness: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :about_me, presence: true
+  validates :encrypted_password, presence: true, confirmation: true, length: {:within => 6..40}, on: :create
 end
