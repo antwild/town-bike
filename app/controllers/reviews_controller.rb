a355b7dc8f523705d217f1ed948c63f26032e778
frederic jonquieres <fjonquieres83@gmail.com>
2019-05-17 11:50:11 +0100

    ReviewsController updated

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,18 @@
 class ReviewsController < ApplicationController
+  def new
+    @bike = Bike.find(params[:bike_id])
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @review.bike = Bike.find(params[:bike_id])
+    @review.save
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:description, :stars, :bike_id, :rider_id)
+  end
 end
