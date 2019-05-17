59bd472adbd1e1dc60ce4653558dcb9790e3901d
Zac Hobbs <zac.hobbs@gmail.com>
2019-05-17 11:16:24 +0100

    review validations added

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :bike
   belongs_to :rider, foreign_key: "rider_id", class_name: "User"
+  validates :stars, presence: true
+  validates :description, presence: true
 end
