598f92e3d8e9d851477f1defce304bab1ba01f2b
frederic jonquieres <fjonquieres83@gmail.com>
2019-05-17 11:49:52 +0100

    bikes controller updated

--- app/controllers/bikes_controller.rb
+++ app/controllers/bikes_controller.rb
@@ -1,6 +1,5 @@
 class BikesController < ApplicationController
   before_action :set_bike, only: [:show, :create, :update]
-  has_many :reviews, dependent: :destroy
   def index
     @bikes = Bike.all
   end
