89ad8b3468a29d8c0ae2f1932014e6403a6f7324
Zac Hobbs <zac.hobbs@gmail.com>
2019-05-22 15:20:30 +0100

    tidy up

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -3,6 +3,12 @@ class PaymentsController < ApplicationController
     @payment = Payment.new
     @booking = Booking.find(params[:booking_id])
     authorize @booking
+    # total_bike = 0
+    # count_bikes = 0
+    # @booking.bike.owner.bikes.each do |bike|
+    #   rating(bike)
+
+    # end
   end
 
   def create
