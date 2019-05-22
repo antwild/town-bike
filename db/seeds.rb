7fe6084d9d585ef79b7d44b2b695e163d38c3213
Jippy Squirmwood <jippysquirmwood@gmail.com>
2019-05-22 08:35:55 +0100

    reviews added to show page and seed attempted to be fixed

--- db/seeds.rb
+++ db/seeds.rb
@@ -200,3 +200,94 @@ puts "Reviews created"
 
 puts 'Finished!'
 
+# require 'faker'
+
+
+# # puts 'Cleaning database...'
+# # Review.destroy_all
+# # Booking.destroy_all
+# Bike.destroy_all
+# User.destroy_all
+
+
+# puts 'Creating users...'
+# users_attributes = [
+#   {
+#     email: "dave@gmail.com",
+#     password: "123456",
+#     password_confirmation: "123456",
+#     first_name: "Dave",
+#     last_name: "Jones",
+#     username: "DJ1",
+#     about_me: "I'm alright",
+#     photo: Cloudinary::Uploader.upload("./app/assets/images/che.jpg")
+#   },
+#   {
+#     email: "Johnny@hotmail.com",
+#     password: "456789",
+#     first_name: "Johnno",
+#     last_name: "Squirmo",
+#     username: "Sandpaper",
+#     about_me: "I'm not from around here",
+#     photo: Cloudinary::Uploader.upload("./app/assets/images/jon.jpg")
+#   },
+#   {
+#     email: "ant@yahoo.com",
+#     password: "789012",
+#     first_name: "Ant",
+#     last_name: "Hero",
+#     username: "DatGuy",
+#     about_me: "I'm just dat guy",
+#     photo: Cloudinary::Uploader.upload("./app/assets/images/ant.jpg")
+#   },
+#   {
+#     email: "fred_fr@gmail.fr",
+#     password: "123456",
+#     first_name: "Fredro",
+#     last_name: "Frank",
+#     username: "FreddyFresh",
+#     about_me: "I like cheese",
+#     photo: Cloudinary::Uploader.upload("./app/assets/images/fred.jpg")
+#   },
+#   {
+#     email: "zapz@hotmail.co.uk",
+#     password: "456789",
+#     password_confirmation: "456789",
+#     first_name: "Zac",
+#     last_name: "Smith",
+#     username: "ZacAttack",
+#     about_me: "Did someone say vape?",
+#     photo: Cloudinary::Uploader.upload("./app/assets/images/zac.jpg")
+#   }
+# ]
+
+# User.create!(users_attributes)
+# puts "Users created"
+
+# # db/seeds.rb
+
+# makes = ['Giant', 'Rally', 'Trek', 'BMC', 'Specialized', 'Raleigh', 'GT']
+# models = ['PX-5', 'F7.3FX', 'GF-01', 'MM-230', 'ZR-150Y', 'GR-1020L', '10.252']
+# bike_types = ['Mountain Bike', 'BMX', 'Road', 'Commuter', 'Hybrid', 'Penny Farthing']
+# descriptions = ['Fast and lightweight', 'robust framed bike', 'fast bike', 'heavy frame', 'downhill specialist', 'great suspension', 'a basket on the front for you']
+# frame_sizes = ['Small', 'Medium', 'Large']
+# foldable = [true, false]
+# bike_photos = ['red']
+
+# puts 'Creating 30 fake bikes...'
+# 5.times do
+#   bike = Bike.create!(
+#     make: makes.sample,
+#     model: models.sample,
+#     color: Faker::Color.color_name,
+#     bike_type: bike_types.sample,
+#     description: descriptions.sample,
+#     frame_size: frame_sizes.sample,
+#     foldable: foldable.sample,
+#     location: "#{Faker::Address.street_address}, #{Faker::Address.city}",
+#     owner_id: 1,
+#     price: rand(10),
+#     photo: Cloudinary::Uploader.upload("/app/assets/images/zac.jpg")
+#   )
+# end
+# puts 'Finished!'
