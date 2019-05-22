8ae91ff9946be8702de0a05d39918cae073cd7be
Zac Hobbs <zac.hobbs@gmail.com>
2019-05-22 15:34:17 +0100

    es5

--- app/assets/javascripts/userShow.js
+++ app/assets/javascripts/userShow.js
@@ -5,45 +5,45 @@ var myBikes = document.querySelectorAll(".my-bikes");
 var bookings = document.querySelectorAll(".bookings-made");
 var editBtn = document.querySelector(".edit-btn");
 
-profileNav.forEach(li => {
-  li.addEventListener("click", () => {
-    profileNav.forEach(li => {
+profileNav.forEach(function(li) {
+  li.addEventListener("click", function() {
+    profileNav.forEach(function() {
       li.classList.remove("profile-active");
     });
     li.classList.add("profile-active");
   });
 });
 
-profileNav[0].addEventListener("click", () => {
+profileNav[0].addEventListener("click", function() {
   removeDetails();
-  myDetails.forEach(detail => {
+  myDetails.forEach(function(detail) {
     detail.style.display = "block";
   });
   editBtn.style.display = "block";
 });
 
-profileNav[1].addEventListener("click", () => {
+profileNav[1].addEventListener("click", function() {
   removeDetails();
-  myBikes.forEach(detail => {
+  myBikes.forEach(function(detail) {
     detail.style.display = "block";
   });
 });
 
-profileNav[2].addEventListener("click", () => {
+profileNav[2].addEventListener("click", function() {
   removeDetails();
-  bookings.forEach(detail => {
+  bookings.forEach(function(detail) {
     detail.style.display = "block";
   });
 });
 
-var removeDetails = () => {
-  myDetails.forEach(detail => {
+var removeDetails = function() {
+  myDetails.forEach(function(detail) {
     detail.style.display = "none";
   });
-  myBikes.forEach(detail => {
+  myBikes.forEach(function(detail) {
     detail.style.display = "none";
   });
-  bookings.forEach(detail => {
+  bookings.forEach(function(detail) {
     detail.style.display = "none";
   });
   editBtn.style.display = "none";
